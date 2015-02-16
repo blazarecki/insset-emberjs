7150959887e0af1e7a8680da75c281e0e6192b8a
Benjamin Lazarecki <benjamin.lazarecki@gmail.com>
2015-02-16 14:30:53 +0100

    scores

--- app/controllers/main.js
+++ app/controllers/main.js
@@ -9,14 +9,20 @@ export default Ember.Controller.extend({
             $('.start').hide();
 
             var that = this;
-            setTimeout(function() {
-                that.transitionToRoute('save', that.get('model'));
-            }, 1000*5);
+            var date = new Date();
+            date.setSeconds(date.getSeconds() + 10);
+            $('#clock').countdown(date)
+                .on('update.countdown', function(e) {
+                    $('.countdown').html(e.offset.seconds);
+                })
+                .on('finish.countdown', function(e) {
+                    that.transitionToRoute('save', that.get('model'));
+                });
         },
 
         click: function() {
             var currentScore = parseInt(this.get('model').get('score'));
             this.get('model').set('score', currentScore+1);
-        },
+        }
     }
 });
